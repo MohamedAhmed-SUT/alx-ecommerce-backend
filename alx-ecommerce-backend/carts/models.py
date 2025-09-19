d95f8f6b25f6bde169e3ecc07b076614a1cdf49b
MohamedAhmed-SUT <mohamedahmed2023sut@gmail.com>
2025-09-19 21:42:14 +0300

    update

--- alx-ecommerce-backend/carts/models.py
+++ alx-ecommerce-backend/carts/models.py
@@ -1,19 +1,24 @@
-# carts/models.py
 from django.db import models
 from django.conf import settings
 from products.models import Product
 
-
 class Cart(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)  # ✅ للتتبع آخر تعديل
+    updated_at = models.DateTimeField(auto_now=True)
+
+    tax = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # ✅ أضفنا tax
 
     def __str__(self):
         return f"Cart {self.id} - {self.user.username}"
 
     def total_price(self):
         return sum(item.total_price() for item in self.items.all())
+
+    @property
+    def total_with_shipping(self):
+        shipping = 5  # يمكن تعديل قيمة الشحن حسب النظام
+        return self.total_price() + shipping + self.tax
 
 
 class CartItem(models.Model):
